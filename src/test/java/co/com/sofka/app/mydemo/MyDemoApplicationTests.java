48ffb420b4664986f3c4e286b06e17e73e341df2
Marlon Vargas <mardrex88@gmail.com>
2022-06-20 15:07:59 -0500

    Creacion del proyecto y se intalan las dependencias slf4j-api y slf4j-jdk14

---
+++ src/test/java/co/com/sofka/app/mydemo/MyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.app.mydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
